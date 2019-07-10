4b0dfc69673fe474eb9876dee4624654fdd79dd5
MohamedEsssam <mohamedessam994@hotmail.com>
2019-07-10 04:28:51 +0200

    Adding soluton

---
+++ CodeForces/B. Equalize Prices/cmake-build-debug/CMakeFiles/B__Equalize_Prices.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/B__Equalize_Prices.dir/main.cpp.o"
+  "B__Equalize_Prices.pdb"
+  "B__Equalize_Prices"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/B__Equalize_Prices.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CodeForces/B. Equalize Prices/cmake-build-debug/CMakeFiles/B__Equalize_Prices.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for B__Equalize_Prices.
+# This may be replaced when dependencies are built.

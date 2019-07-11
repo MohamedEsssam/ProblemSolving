7937c73b3b5f2d3839ce63acd1671f910113b515
MohamedEsssam <mohamedessam994@hotmail.com>
2019-07-11 05:41:32 +0200

    Adding soluton

--- CodeForces/B. Equalize Prices/main.cpp
+++ CodeForces/B. Equalize Prices/main.cpp
@@ -1,5 +1,27 @@
 #include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
 
 int main() {
-    return 0;
+    vector <int>vector;
+    int q,k,n;
+    int data;
+    scanf("%d",&q);
+    while (q > 0){
+        scanf("%d",&n);
+        scanf("%d",&k);
+        for (int i = 0; i <n ; ++i) {
+            scanf("%d",&data);
+            vector.push_back(data);
+        }
+        sort(vector.begin(), vector.end());
+     //   printf("(%d) %d-%d = %d\t",2*k ,vector.at(vector.size()-1), vector.at(0), vector.at(vector.size()-1)-vector.at(0));
+        if (vector.at(vector.size()-1)-vector.at(0) > 2*k) printf("-1\n");
+        else printf("%d\n",vector.at(0)+k);
+
+        vector.clear();
+        q--;
+    }
 }

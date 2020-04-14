dcd8c89c0f1d09682969dcfc7f181e445eede946
MohamedEsssam <me547010@gmail.com>
2020-04-14 14:16:23 +0200

    add second week problems solutions

--- 30-dayLeetCode/src/Main.java
+++ 30-dayLeetCode/src/Main.java
@@ -3,8 +3,10 @@ import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
         Solution solution = new Solution();
-        int[] arr = {1,1,2,2,3};
+        int[] arr = {0,1,1};
+        int[][] items = {{1,1},{1,1},{0,2},{1,3}};
         String []strs = {"eat", "tea", "tan", "ate", "nat", "bat"};
+
 //        System.out.println(Arrays.toString(solution.unlucky(arr, 3)));
 //        System.out.println(solution.singleNumber(arr));
 //        System.out.println(solution.isHappy(19));
@@ -12,9 +14,10 @@ public class Main {
 //        solution.moveZeroes(arr);
 //        System.out.println(solution.maxProfit(arr));
 //        System.out.println(solution.groupAnagrams(strs));
-        System.out.println(solution.countElements(arr));
-
-
-
+//        System.out.println(solution.countElements(arr));
+//        System.out.println(solution.backspaceCompare("a#c", "b"));
+//        System.out.println(solution.lastStoneWeight(arr));
+//        System.out.println(solution.findMaxLength(arr));
+//        System.out.println(solution.stringShift("abcdefg", items));
     }
 }
--- 30-dayLeetCode/src/Solution.java
+++ 30-dayLeetCode/src/Solution.java
@@ -22,9 +22,12 @@ public class Solution {
         return newArr;
     }
 
+/**************************************************************************
+ **********************    WEEk 1      ************************************
+ *************************************************************************/
+
     /**
      * week 1 problem 1 single number
-     *
      * @param nums
      * @return
      */
@@ -158,6 +161,195 @@ public class Solution {
         return count;
     }
 
+    /**************************************************************************
+     **********************    WEEk 2      ************************************
+     *************************************************************************/
+
+    /**
+     * week 2 problem 1  Middle of the Linked List
+     * @param //head
+     * @return
+     */
+    public class ListNode {
+      int val;
+      ListNode next;
+      ListNode(int x) { val = x; }
+  }
+    public ListNode middleNode(ListNode head) {
+        var first = head;
+        var second = head;
+        while (second != null && second.next != null){
+            first = first.next;
+            second = second.next.next;
+        }
+
+        return first;
+    }
+
+    /**
+     * week 2 problem 2  backspace Compare
+     * @param S
+     * @param T
+     * @return
+     */
+    public boolean backspaceCompare(String S, String T) {
+        Stack<Character> str1 = new Stack<>();
+        Stack<Character> str2 = new Stack<>();
+
+        for (char ch : S.toCharArray()) {
+            if (ch == '#' && !str1.isEmpty())
+                str1.pop();
+            else if (ch == '#' && str1.isEmpty())
+                continue;
+            else str1.push(ch);
+        }
+
+        for (char ch : T.toCharArray()) {
+            if (ch == '#' && !str2.isEmpty())
+                str2.pop();
+            else if (ch == '#' && str2.isEmpty())
+                continue;
+            else str2.push(ch);
+        }
+
+        System.out.println(str1+" "+str2);
+        if(str1.size() != str2.size())
+            return false;
+
+        while (str1.size()!=0 && str2.size() !=0)
+            if (str1.pop() != str2.pop())
+                return false;
+
+        return true;
+    }
+
+    /**
+     * week 2 problem 3  Min Stack
+     */
+    public class MinStack {
+
+    Stack<Integer> stack, minStack;
+
+    public MinStack() {
+        stack = new Stack<>();
+        minStack = new Stack<>();
+    }
+
+    public void push(int x) {
+        stack.push(x);
+        
+        if (minStack.empty())
+            minStack.push(x);
+        else if (x <= minStack.peek())
+            minStack.push(x);
+    }
+
+    public void pop() {
+          if (stack.isEmpty())
+            throw new IllegalStateException();
+        
+        int top = stack.pop();
+        if (!minStack.empty() && minStack.peek() == top) {
+            minStack.pop();
+        }
+    }
+
+    public int top() {
+        return stack.peek();
+    }
+
+    public int getMin() {
+        return minStack.peek();
+    }
+}   
+
+    /**
+     * week 2 problem 4 diameter Of BinaryTree
+     * @param //root
+     * @return
+     */
+    public class TreeNode {
+      int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode(int x) { val = x; }
+    }
+    private int count;
+    public int diameterOfBinaryTree(TreeNode root) {
+       count = 0;
+       depth(root);
+        
+       return count ; 
+    }
+
+    /**
+     * week 2 problem 5 Last Stone Weight
+     * @param stones
+     * @return
+     */
+    public int lastStoneWeight(int[] stones) {
+        PriorityQueue<Integer> priorityStones = new PriorityQueue<>(
+                                                    Comparator.reverseOrder());
+        for (int stone : stones)
+            priorityStones.add(stone);
+
+        while (priorityStones.size() > 1){
+            int first = priorityStones.remove();
+            int second = priorityStones.remove();
+            if (first != second)
+                priorityStones.add(first-second);
+        }
+        return priorityStones.size() == 1 ? priorityStones.remove() : 0;
+    }
+
+    /**
+     * week 2 problem 6 Find Max Length
+     * @param nums
+     * @return
+     */
+    public int findMaxLength(int[] nums) {
+        if (nums.length <= 1)
+            return 0;
+
+        HashMap<Integer, Integer> cacheMap = new HashMap<>();
+        cacheMap.put(0, -1);
+
+        int max = 0, i = 0, count = 0;
+        for (int num : nums) {
+            count = num == 1 ? count+1 : count-1;
+
+            if (cacheMap.containsKey(count))
+                max = Math.max(max, i - cacheMap.get(count));
+            else
+                cacheMap.put(count, i);
+
+            i++;
+        }
+
+        return max;
+    }
+
+    /**
+     * week 2 problem 7 Perform String Shifts
+     * @param s
+     * @param shift
+     * @return
+     */
+    public String stringShift(String s, int[][] shift) {
+        String newStr = "";
+        for (var item : shift) {
+            if (item[0]==0)
+                s = leftRotate(s, item[1]);
+            else
+                s = rightRotate(s, item[1]);
+        }
+
+        return s;
+    }
+
+/**************************************************************************
+ **********************    private methods      ***************************
+ *************************************************************************/
 
     /**
      * private methods
@@ -167,5 +359,31 @@ public class Solution {
         Arrays.sort(letters);
 
         return new String(letters);
+    }
+
+    private int depth(TreeNode root) {
+       if (root == null)
+         return 0;
+
+       if (isLeaf(root))
+         return 1; 
+        
+       int left = depth(root.left);
+       int right = depth(root.right);
+       count = Math.max(count, left + right);
+       return 1 + Math.max(left, right);
+    }
+
+    private boolean isLeaf(TreeNode node) {
+        return node.left == null && node.right == null;
+    }
+
+    private String leftRotate(String str, int d) {
+        String ans = str.substring(d) + str.substring(0, d);
+        return ans;
+    }
+
+    private String rightRotate(String str, int d) {
+        return leftRotate(str, str.length() - d);
     }
 }
